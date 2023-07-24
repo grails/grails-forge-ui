be46bf0e9c8af91e220e10e09547995856e862c5
Puneet Behl <behlp@objectcomputing.com>
2023-07-24 22:39:45 +0530

    Update AltFooter.jsx

--- app/launch/src/components/AltFooter/AltFooter.jsx
+++ app/launch/src/components/AltFooter/AltFooter.jsx
@@ -8,54 +8,38 @@ import Youtube from '../../images/youtube.svg'
 const AltFooter = () => (
   <footer>
     <div class="content">
-      <div class="ocihometograils">
-        <span>Sponsored by</span>
-        <a href="https://objectcomputing.com/products/grails/">
-          <img
-            src={OciHomeToGrails}
-            alt="Object Computing - Home to Grails"
-            width="300px"
-          />
-        </a>
-      </div>
-      <a href="https://slack.grails.org">
-        <img src={Slack} class="" alt="Slack Icon" />
-      </a>
-      <a href="https://www.youtube.com/watch?v=XnRNfDGkBVg&amp;list=PLI74De5M9T73uH3WilDCePP2qfSDpMaGu">
-        <img src={Youtube} class="" alt="Youtube Icon" />
-      </a>
-      <a href="https://www.linkedin.com/groups/39757">
-        <img src={Linkedin} class="" alt="LinkedIn Icon" />
-      </a>
-      <a href="https://github.com/grails/">
-        <img src={Github} class="" alt="Github Icon" />
-      </a>
-      <a href="https://twitter.com/grailsframework">
-        <img src={Twitter} class="" alt="Twitter Icon" />
-      </a>
-      <section class="partnersnav">
-        <div class="links">
-          Grails' repositories are hosted by{' '}
-          <a href="https://grails.jfrog.io/">Artifactory</a>
+        <div class="ocihometograils">
+            <a href="https://grails.org/foundation/index.html"><img class="" src="https://grails.org/images/foundation-logo-horizontal-orange.svg" alt="Grails Foundation" width="300px"></a>
+            <span style="margin-top:22px;">© 2023 Grails Foundation. All rights reserved.</span>
         </div>
-        <div class="links">
-          Website hosting provided by <a href="https://aws.amazon.com/">AWS</a>
-        </div>
-        <div class="links">
-          YourKit supports Grails with its{' '}
-          <a href="https://www.yourkit.com/java/profiler/"> Java Profiler</a>
-        </div>
-        <div class="links">
-          Grails is Open Source{' '}
-          <a href="https://www.apache.org/licenses/LICENSE-2.0.html">
-            Apache 2 License
-          </a>
-        </div>
-        <div class="links">
-          <a href="https://grails.org/buildstatus.html">Build Status</a>
-        </div>
-      </section>
+    
+        <a href="mailto:info@grails.org"><img class="" src="https://grails.org/images/email.svg" alt="Email Icon"></a>
+        <a href="https://slack.grails.org"><img class="" src="https://grails.org/images/slack.svg" alt="Slack Icon"></a>
+        <a href="https://www.youtube.com/watch?v=XnRNfDGkBVg&amp;list=PLI74De5M9T73uH3WilDCePP2qfSDpMaGu"><img class=""
+                                                                                                               src="https://grails.org/images/youtube.svg"
+                                                                                                               alt="Youtube Icon"></a>
+        <a href="https://www.linkedin.com/showcase/official-grails/"><img class=""
+                                                                          src="https://grails.org/images/linkedin.svg"
+                                                                          alt="LinkedIn Icon"></a>
+        <a href="https://github.com/grails/"><img class="" src="https://grails.org/images/github.svg" alt="Github Icon"></a>
+        <a href="https://twitter.com/grailsframework"><img class="" src="https://grails.org/images/twitter.svg"
+                                                           alt="Twitter Icon"></a>
+    
+        <section class="partnersnav">
+    
+            <div class="links">The Grails framework is Open Source
+                <a href="https://www.apache.org/licenses/LICENSE-2.0.html">Apache 2 License</a>
+            </div>
+    
+            <div class="links">
+                <a href="https://grails.org/privacy-policy.html">Privacy Policy</a>
+            </div>
+            <div class="links">
+                <a href="https://grails.org/trademark-policy.html">Trademark Policy &amp; Brand Guidelines</a>
+            </div>
+        </section>
     </div>
+
   </footer>
 )
 
